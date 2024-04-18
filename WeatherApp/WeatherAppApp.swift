7ffef8aebde6452e5bb0b7816527e215fd1687e1
Hardik Bhawsar <hardik.13bhawsar@gmail.com>
2024-04-18 00:44:23 -0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Hardik Bhawsar on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
